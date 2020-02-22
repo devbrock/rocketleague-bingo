ecf9d0e6405133f08d0a2fad823fcd2f101e9aea
DevBrock <tbrockb@gmail.com>
2020-02-22 12:20:34 -0600

    update admin login

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>Admin Page</h1>
+  <div class="flex flex-col max-w-xs justify-between">
+    <h1>Login</h1>
+    <input type="text" placeholder="username" class="px-4 py-2 bg-gray-200 text-gray-900 rounded border focus:outline-none my-2" />
+    <input type="text" placeholder="password" class="px-4 py-2 bg-gray-200 text-gray-900 rounded border focus:outline-none my-2" />
+    <button class="px-4 py-2 bg-blue-900 text-white rounded border focus:outline-none my-2">Login</button>
+  </div>
 </template>
 
 <script>
