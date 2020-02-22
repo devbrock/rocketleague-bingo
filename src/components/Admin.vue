6180d9518911b237a552f53bdeb03fa2ed444b14
DevBrock <tbrockb@gmail.com>
2020-02-22 10:16:00 -0600

    set up routing

---
+++ src/components/Admin.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Admin Page</h1>
+</template>
+
+<script>
+export default {
+  name: "Admin"
+};
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import BingoCard from './components/BingoCard'
+import Admin from './components/Admin'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { path: '', component: BingoCard },
+    { path: '/admin', component: Admin },
+  ],
+  mode: 'history'
+});
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
